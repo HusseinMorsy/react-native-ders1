987c30748eb6a134129fc0fbc33cff13618fadeb
Hussein Morsy <hussein@morsy.de>
2018-09-29 13:34:02 +0200

    Extract Stars component

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+function Stars({ name, count }) {
+  const blackStars = "★".repeat(count);
+  const whiteStars = "☆".repeat(5 - count);
+  return (
+    <Text>
+      {name}: {blackStars}
+      {whiteStars}
+    </Text>
+  );
+}
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello World</Text>
+        <Stars name="Luke" count={3} />
+        <Stars name="Han" count={1} />
+        <Stars name="Leia" count={5} />
+        <Stars name="Nobody" count={0} />
       </View>
     );
   }
